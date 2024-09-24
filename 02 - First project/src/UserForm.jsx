270800ca696a3b9cd89316ac48fc00cdf73e7853
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-24 12:38:15 +0200

    another way to select specific inputs

--- 02 - First project/src/UserForm.jsx
+++ 02 - First project/src/UserForm.jsx
@@ -13,12 +13,12 @@ export const UserForm = ({onUserAdd}) => {
   return (
     <form onSubmit={handleSubmit}>
         <div>
-            <label>Name</label>
-            <input type="text"  value={name} onChange={e => setName(e.target.value)}/>
+            <label htmlFor="name">Name</label>
+            <input id="name" type="text"  value={name} onChange={e => setName(e.target.value)}/>
         </div>
         <div>
-            <label>Email</label>
-            <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+            <label htmlFor="email">Email</label>
+            <input id="email" type="text" value={email} onChange={e => setEmail(e.target.value)}/>
         </div>
         <button type="submit">Add User</button>
     </form>
