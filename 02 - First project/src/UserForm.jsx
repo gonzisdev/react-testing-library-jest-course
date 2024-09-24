b27e2ba95c2318771f2028ce9f9953c121bbd71d
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-24 11:37:13 +0200

    first project and first test

---
+++ 02 - First project/src/UserForm.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+
+export const UserForm = ({onUserAdd}) => {
+
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onUserAdd({name, email})
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label>Name</label>
+            <input type="text"  value={name} onChange={e => setName(e.target.value)}/>
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+        </div>
+        <button>Add User</button>
+    </form>
+  )
+}
