8d607aae2082365e3bbf3a400c7e897cc32cdb56
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-24 12:30:00 +0200

    second test

--- 02 - First project/src/UserForm.jsx
+++ 02 - First project/src/UserForm.jsx
@@ -20,7 +20,7 @@ export const UserForm = ({onUserAdd}) => {
             <label>Email</label>
             <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
         </div>
-        <button>Add User</button>
+        <button type="submit">Add User</button>
     </form>
   )
 }
