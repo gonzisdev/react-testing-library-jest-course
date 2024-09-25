014c2ba328806d4c1e2ba7d63db024b523b026ce
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-25 17:04:48 +0200

    testing the link

--- 03 - codesplain/src/components/repositories/RepositoriesListItem.test.js
+++ 03 - codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -15,7 +15,9 @@ function renderComponent() {
         full_name: 'facebook/react',
         language: 'Javascript', 
         description: 'A js library',
-        owner: 'facebook', 
+        owner: {
+            login: 'facebook'
+        }, 
         name: 'react',
         html_url: 'https://github.com/facebook/react'
     }
@@ -56,4 +58,11 @@ test('shows a fileicon with the appropiate icon', async () => {
     renderComponent()
     await screen.findByRole('img', {name: 'Javascript'})
     expect(icon).toHaveClass('js-icon')
+})
+
+test('shows a link to the code editor page', async () => {
+    const { repository } = renderComponent()
+    await screen.findByRole('img', {name: 'Javascript'})
+    const link = await screen.findByRole('link', {name: new RegExp(repository.owner.login)})
+    expect(link).toHaveAttribute('href', `/repositories/${repository.full_name}`)
 })
