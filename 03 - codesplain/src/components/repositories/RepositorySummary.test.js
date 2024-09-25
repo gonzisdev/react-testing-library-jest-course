0490e463c074da8798ce4fdea39a957bad56e3e2
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-25 11:52:33 +0200

    another text with flexible queries

--- 03 - codesplain/src/components/repositories/RepositorySummary.test.js
+++ 03 - codesplain/src/components/repositories/RepositorySummary.test.js
@@ -14,3 +14,20 @@ test('displays the primary language of the repository', () => {
 
     expect(language).toBeInTheDocument()
 })
+
+test('displays information about the repository', () => {
+    const repository = {
+        language: 'JavaScript',
+        stargazers_count: 5,
+        forks: 30,
+        open_issues: 1
+    }
+    render(<RepositoriesSummary repository={repository} />)
+
+    for (const key in repository) {
+        const value = repository[key]
+        const element = screen.getByText(new RegExp(value))
+
+        expect(element).toBeInTheDocument()
+    }
+})
