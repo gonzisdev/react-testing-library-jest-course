df828ade09e5d14c0c1005008981d787f1c3aecb
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-26 11:54:43 +0200

    limiting text execution describe.only test.only

--- 03 - codesplain/src/components/auth/AuthButtons.test.js
+++ 03 - codesplain/src/components/auth/AuthButtons.test.js
@@ -30,7 +30,7 @@ describe('when user is not signed in', () => {
         expect(signInButton).toBeInTheDocument()
         expect(signInButton).toHaveAttribute('href', '/signin')
         expect(signUpButton).toBeInTheDocument()
-        expect(signUpButton).toHaveAttribute('href', '/signun')
+        expect(signUpButton).toHaveAttribute('href', '/signup')
     })
     
     test('sign out is not visible', async () => {
@@ -51,10 +51,23 @@ describe('when user is signed in', () => {
     ])
     
     test('sign in and sign up are not visible', async () => {
-        renderComponent()
+        await renderComponent()
+        const signInButton = screen.queryByRole('link', {
+            name: /sign in/i
+        })
+        const signUpButton = screen.queryByRole('link', {
+            name: /sign up/i
+        })
+        expect(signInButton).not.toBeInTheDocument()
+        expect(signUpButton).not.toBeInTheDocument()
     })
     
     test('sign out is visible', async () => {
-        renderComponent()
+        await renderComponent()
+        const signOutButton = screen.getByRole('link', {
+            name: /sign out/i
+        })
+        expect(signOutButton).toBeInTheDocument()
+        expect(signOutButton).toHaveAttribute('href', '/signout')
     })
 })
