e8374c7b0b8064dc042191a6ff7420bdfa01beeb
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-26 10:37:44 +0200

    reusable create server function

--- 03 - codesplain/src/routes/HomeRoute.test.js
+++ 03 - codesplain/src/routes/HomeRoute.test.js
@@ -1,34 +1,23 @@
 import { render, screen } from "@testing-library/react"
-import { setupServer } from "msw/node"
-import { rest } from "msw"
 import { MemoryRouter } from "react-router"
 import HomeRoute from "./HomeRoute"
+import { createServer } from "../test/server"
 
-const handlers = [
-    rest.get('/api/repositories', (req, res, ctx) => {
-        const query = req.url.searchParams.get('q')
-        return res(
-            ctx.json({
+createServer([
+    {
+        path: '/api/repositories',
+        res: 'get',
+        res: (req) => {
+            const language = req.url.searchParams.get('q').split('language:')[1]
+            return {
                 items: [
-                    {id: 1, full_name: 'full name!!!'},
-                    {id: 2, full_name: 'other name!!!'}
+                    {id: 1, full_name: `${language}_one`},
+                    {id: 2, full_name: `${language}_two`}
                 ]
-            })
-        )
-    })
-]
-
-const server = setupServer(...handlers)
-
-beforeAll(() => {
-    server.listen()
-})
-afterEach(() => {
-    server.resetHandlers()
-})
-afterAll(() => {
-    server.close()
-})
+            }
+        }
+    }
+])
 
 test('renders two links for each language', async () => {
     render(
@@ -36,8 +25,25 @@ test('renders two links for each language', async () => {
             <HomeRoute />
         </MemoryRouter>
     )
-    //screen.debug()
     // Loop over each language
-    // For each language, mnake sure we see two links
-    // Assert that the links have the appropiate full_name
+    const languages = [
+        'javascript',
+        'typescript',
+        'rust',
+        'go',
+        'python',
+        'java'
+    ]
+    for (const language of languages) {
+        // For each language, mnake sure we see two links
+        const links = await screen.findAllByRole('link', {
+            name: new RegExp(`${language}_`)
+        })
+        expect(links).toHaveLength(2)
+        // Assert that the links have the appropiate full_name
+        expect(links[0]).toHaveTextContent(`${language}_one`)
+        expect(links[1]).toHaveTextContent(`${language}_two`)
+        expect(links[0]).toHaveAttribute('href', `/repositories/${language}_one`)
+        expect(links[1]).toHaveAttribute('href', `/repositories/${language}_two`)
+    }
 })
