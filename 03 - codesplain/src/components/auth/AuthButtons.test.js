31ef2e1943cff8c37abe3193c366affe03510ea0
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-26 11:15:45 +0200

    scoping test hooks

--- 03 - codesplain/src/components/auth/AuthButtons.test.js
+++ 03 - codesplain/src/components/auth/AuthButtons.test.js
@@ -3,31 +3,35 @@ import { MemoryRouter } from "react-router"
 import { createServer } from "../../test/server"
 import AuthButtons from "./AuthButtons"
 
-// createServer() ---> GET 'api/users' ---> {user: null}
-createServer([
-    {path: 'api/user', res: () => {
-        return { user: null }
-    }}
-])
-test('when user is not signed in, sign in and sign up are visible', async () => {
-
+describe('when user is not signed in', () => {
+    // createServer() ---> GET 'api/users' ---> {user: null}
+    createServer([
+        {path: 'api/user', res: () => {
+            return { user: null }
+        }}
+    ])
+    test('when user is not signed in, sign in and sign up are visible', async () => {
+    
+    })
+    
+    test('when user is not signed in, sign out is not visible', async () => {
+        
+    })
 })
 
-test('when user is not signed in, sign out is not visible', async () => {
+describe('when user is signed in', () => {
+    // createServer() ---> GET 'api/users' ---> {user: {id: 3, email: 'asdf@asdf.com'}}
+    createServer([
+        {path: 'api/user', res: () => {
+            return { user: {id: 3, email: 'asdf@asdf.com'} }
+        }}
+    ])
     
-})
-
-// createServer() ---> GET 'api/users' ---> {user: {id: 3, email: 'asdf@asdf.com'}}
-createServer([
-    {path: 'api/user', res: () => {
-        return { user: {id: 3, email: 'asdf@asdf.com'} }
-    }}
-])
-
-test('when user issigned in, sign in and sign up are not visible', async () => {
+    test('when user issigned in, sign in and sign up are not visible', async () => {
+        
+    })
     
-})
-
-test('when user is signed in, sign out is visible', async () => {
-    
+    test('when user is signed in, sign out is visible', async () => {
+        
+    })
 })
